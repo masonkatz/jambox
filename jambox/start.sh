ee0d49b801ae0d82409d3af57f2368a05207660b
Mason J. Katz <Mason.Katz@gmail.com>
2019-03-13 13:26:44 -0700

    nuke base image and start squiding

--- jambox/start.sh
+++ jambox/start.sh
@@ -10,5 +10,8 @@ echo
 
 echo ${JAMBOX_PASSWORD} | passwd user --stdin
 
-ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N ''
-/usr/sbin/sshd -D
+ssh-keygen -t rsa     -f /etc/ssh/ssh_host_rsa_key     -N ''
+ssh-keygen -t ecdsa   -f /etc/ssh/ssh_host_ecdsa_key   -N ''
+ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N ''
+
+/usr/sbin/sshd -D -e
--- jammer/start.sh
+++ jammer/start.sh
@@ -8,7 +8,10 @@ echo
 echo
 echo
 
-connect="autossh -M 0 -N -oStrictHostKeyChecking=no -p${JAMBOX_SSH_PORT} -R${JAMBOX_PORT}:localhost:${JAMMER_PORT} ${JAMBOX_USER}@${JAMBOX_HOST}"
+
+connect="autossh -M 0 -N -oStrictHostKeyChecking=no -p${JAMBOX_SSH_PORT} -R3128:localhost:3128 -R${JAMBOX_PORT}:localhost:${JAMMER_PORT} ${JAMBOX_USER}@${JAMBOX_HOST}"
+
+/usr/sbin/squid -N &
 
 socat tcp-listen:${JAMMER_PORT},fork tcp:host.docker.internal:22 &
 
