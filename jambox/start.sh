9f9bc9c4e93446a614a829cbac6dcd417df73796
Mason J. Katz <Mason.Katz@gmail.com>
2019-03-15 16:55:35 -0700

    almost there

--- jambox/start.sh
+++ jambox/start.sh
@@ -8,12 +8,10 @@ echo
 echo
 echo
 
-echo ${JAMBOX_PASSWORD} | passwd user --stdin
+echo ${JAMBOX_PASSWORD} | passwd jb --stdin
 
 ssh-keygen -t rsa     -f /etc/ssh/ssh_host_rsa_key     -N ''
 ssh-keygen -t ecdsa   -f /etc/ssh/ssh_host_ecdsa_key   -N ''
 ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N ''
-
-socat tcp-listen:${JAMBOX_OUT},fork tcp:host.docker.internal:2222 &
 
 /usr/sbin/sshd -D -e
