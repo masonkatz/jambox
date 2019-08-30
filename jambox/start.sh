a4dc4b4813382d1c23f102310e85ab6082fa7f28
Mason J. Katz <Mason.Katz@gmail.com>
2019-08-30 13:55:53 -0700

    Jambox holds keys and homedir on a volume

--- jambox/start.sh
+++ jambox/start.sh
@@ -8,10 +8,34 @@ echo
 echo
 echo
 
-echo ${JAMBOX_PASSWORD} | passwd jb --stdin
+# jambox-config volume holds:
+#
+# Host ssh keys so we can bring the jambox up/down and not
+# change identity on the jammer. This lets us only run the jambox when
+# we need it (jammer will re-attach w/in a minute)
+#
+# User (only one) home directory, this is mainly for the ~/.ssh
+# authorized_keys, but this also makes the jambox a usefull place to
+# drop files and preserve after stopping the jambox.
 
-ssh-keygen -t rsa     -f /etc/ssh/ssh_host_rsa_key     -N ''
-ssh-keygen -t ecdsa   -f /etc/ssh/ssh_host_ecdsa_key   -N ''
-ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N ''
+CFGDIR=/config
+for key in rsa ecdsa ed25519; do
+	if [ ! -e $CFGDIR/ssh_host_${key}_key ]; then
+		ssh-keygen -t $key -f $CFGDIR/ssh_host_${key}_key -N ''
+	fi
+	ln -s $CFGDIR/ssh_host_${key}_key     /etc/ssh/
+	ln -s $CFGDIR/ssh_host_${key}_key.pub /etc/ssh/
+done
+
+if [ ! -e $CFGDIR/home ]; then
+	mkdir $CFGDIR/home
+fi
+rm -rf /home
+ln -s $CFGDIR/home /home
+
+
+useradd $JAMBOX_USER
+echo $JAMBOX_PASSWORD | passwd $JAMBOX_USER --stdin
+
 
 /usr/sbin/sshd -D -e
